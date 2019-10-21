42a657adba37ff9f67494396bfaeba3926081f41
JingHe <weifujun@sailvan.com>
2019-10-21 11:16:45 +0800

    日志对象引入更正

--- web-console/src/main/java/com/web/console/controller/TestController.java
+++ web-console/src/main/java/com/web/console/controller/TestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 import java.util.Map;
 
 /**
